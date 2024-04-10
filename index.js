1984661062dbcc80541ebc14b003a84e13e5dd01
Thinh Nguyen <tnguy19@bu.edu>
2024-04-10 17:37:26 -0400

    App setup

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
+
+app.post("/submit", (req, res) => {
+  
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
