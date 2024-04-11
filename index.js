da17682881908f1f2db6b94f56e50a7157ebe1ba
Thinh Nguyen <tnguy19@bu.edu>
2024-04-11 16:49:35 -0400

    Add post edit feature (bugs unresolved)

--- index.js
+++ index.js
@@ -10,21 +10,49 @@ app.use(express.static("public"));
 let posts = [];
 
 app.get("/", (req, res) => {
-  res.render("index.ejs");
-  posts = [];
+  //console.log(req.body)
+  res.render("index.ejs", {
+    posts: posts
+  });
 });
 
 app.post("/submit", (req, res) => {
-  console.log(req.body);
-  if (req.body.postTitle && req.body.subTitle && req.body.postContent){
-    posts.push(req.body);
+  //console.log(req.body);
+  if (req.body.postTitle && req.body.subTitle && req.body.postContent) {
+      const newPost = {
+          id: Date.now().toString(), 
+          postTitle: req.body.postTitle,
+          subTitle: req.body.subTitle,
+          postContent: req.body.postContent
+      };
+      posts.push(newPost);
   }
-  console.log(posts);
-  res.render("index.ejs", {
-    posts: posts
-  })
+  res.redirect("/");
+});
+
+app.get("/edit/:id", (req, res) => {
+  const postId = req.params.id;
+  const postIndex = posts.findIndex(post => post.id === postId);
+  if (postIndex !== -1) {
+    res.render("edit.ejs", { post: posts[postIndex] });
+  } else {
+    res.send("Post not found");
+  }
+});
+
+app.post("/edit/:id", (req, res) => {
+  const postId = req.params.id;
+  const updatedPost = req.body;
+  const postIndex = posts.findIndex(post => post.id === postId);
+  if (postIndex !== -1) {
+      posts[postIndex] = updatedPost;
+      res.redirect("/");
+  } else {
+    res.send("Post not found");
+  }
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
+  //console.log(posts);
 });
