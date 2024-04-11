6662e2f0bac70c6d632a3fe768bb7c6b91a93dbe
Thinh Nguyen <tnguy19@bu.edu>
2024-04-11 14:12:34 -0400

    Add functionality to create multiple posts

--- index.js
+++ index.js
@@ -7,12 +7,22 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts = [];
+
 app.get("/", (req, res) => {
   res.render("index.ejs");
+  posts = [];
 });
 
 app.post("/submit", (req, res) => {
-  
+  console.log(req.body);
+  if (req.body.postTitle && req.body.subTitle && req.body.postContent){
+    posts.push(req.body);
+  }
+  console.log(posts);
+  res.render("index.ejs", {
+    posts: posts
+  })
 });
 
 app.listen(port, () => {
